e5f5f139004b7388a666e7839f5d3fd2772d4c27
Alex V. Malikov <malikov.soft@yandex.ru>
2019-03-19 12:20:34 +0300

    20190319

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/MalikovSoft/coverted_ctx_links_validator/database"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 func main() {
+	db, err := database.InitDatabase(`root@/ncfu?charset=utf8&parseTime=true&loc=Local`)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
 }
